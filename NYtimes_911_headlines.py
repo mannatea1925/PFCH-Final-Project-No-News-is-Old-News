167a6bff272d056f51d6f1053d7aace8043394d0
mannatea1925 <122116380+mannatea1925@users.noreply.github.com>
2023-05-09 21:26:56 -0400

    Update NYtimes_911_headlines.py

--- NYtimes_911_headlines.py
+++ NYtimes_911_headlines.py
@@ -1,13 +1,15 @@
-
 import requests
 import json
+import time
+import math
 
 base_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 payload = {
-    'api-key': ' ',
-    'q': '9/11',
+    'api-key': '',
+    'q':'9/11',
     'begin_date': '20220911',
-    'end_date': '20220918'
+    'end_date': '20220918',
+    'page':1
 }
 
 save_data=[]
@@ -16,9 +18,45 @@ r = requests.get (base_url, params=payload)
 
 data = json.loads(r.text)
 
-for item in data['response']['docs']:
-    if 'headline' in item:
-        print (item['headline'])
 
-with open ('911_headlines.json','w') as out:
-    json.dump (data, out, indent=2)
+# the total number of 'hits' is stored here
+hits = data['response']['meta']['hits']
+
+# if 10 hits per page then that means
+total_pages = hits / 10
+
+# if there is a remainder then we need to round up
+# we can use math.ceil to round up to the nearest whole number
+total_pages = math.ceil(total_pages)
+
+print("There are:",total_pages,'total pages')
+
+
+# # so loop that many times, its not inclusive so we add +1 to the end
+for page in range(1,total_pages +1):
+
+    print('Doing page',page)
+
+    payload = {
+        'api-key': '',
+        'q':'9/11',
+        'begin_date': '20220911',
+        'end_date': '20220918',
+        'page':page
+    }
+    r = requests.get (base_url, params=payload)
+    data = json.loads(r.text)
+
+    # add in the results to our results to save_data list
+    save_data = save_data + data['response']['docs']
+
+    time.sleep(12)
+
+    for item in data['response']['docs']:
+        if 'headline' in item:
+            print (item['headline'])
+        if 'print_page' in item:
+            print (item['print_page'])
+
+with open ('911_headlines.json', 'w') as out:
+    json.dump(save_data, out, indent = 2)
